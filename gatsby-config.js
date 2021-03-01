61172e4a95fead69d0dfe9cd484d404d0fe0aedb
Noboru Yoshida <noboruyoshida1997@gmail.com>
2021-03-01 18:25:46 -0500

    Add skeleton code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
-  siteMetadata: {
-    title: "JSAM Link Page",
-  },
-  plugins: ["gatsby-plugin-offline"],
+    siteMetadata: {
+        title: 'JSAM Link Page',
+    },
+    plugins: [
+        'gatsby-plugin-offline',
+        {
+            resolve: 'gatsby-source-google-spreadsheets',
+            options: {
+                spreadsheetId: '103_VfM07hiIqODBf7_LrHYDKsp-luhOEj43YBo0KBp0',
+                worksheetTitle: 'sheet1',
+                typePrefix: 'GoogleSpreadsheet',
+                apiKey: 'AIzaSyDPGCtDyd4Minlj7lc6NLF2rIZOlQLK02g',
+            },
+        },
+    ],
 };
