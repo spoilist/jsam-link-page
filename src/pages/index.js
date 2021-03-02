ac3a3eff553dad45de7b1ea629c024598ab943b9
Noboru Yoshida <noboruyoshida1997@gmail.com>
2021-03-02 12:44:30 -0500

    Add styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,42 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
-
-// styles
-const pageStyles = {
-    color: '#232129',
-    padding: '96px',
-    fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-const headingStyles = {
-    marginTop: 0,
-    marginBottom: 64,
-    maxWidth: 320,
-};
+import styles from './index.module.css';
 
 // markup
 const IndexPage = ({ data }) => {
     const links = data.allGoogleSheet1Sheet.nodes;
+    const categorizedLinks = groupByCategory(links);
 
     return (
-        <main style={pageStyles}>
+        <main className={styles.Page}>
             <title>JSA Montreal | Link page</title>
-            <h1 style={headingStyles}>Welcome to JSA Montreal's social platforms</h1>
-            {links.map((link) => (
-                <div key={link.id}>
-                    <h2>{link.title}</h2>
-                    <p>{link.category}</p>
-                    <p>{link.url}</p>
-                </div>
-            ))}
+            <h2 className={styles.Heading}>Welcome to JSA Montreal's social platforms</h2>
+            <div className={styles.LinkBlocks}>
+                {Object.keys(categorizedLinks).map((category) => (
+                    <div key={category} className={styles.LinkBlock}>
+                        <h3 className={styles.CategoryTitle}>{category}</h3>
+                        {categorizedLinks[category].map((linkObject) => (
+                            <a
+                                key={linkObject.id}
+                                href={linkObject.url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className={styles.Link}
+                            >
+                                {linkObject.title}
+                            </a>
+                        ))}
+                    </div>
+                ))}
+                {/* {links.map((link) => (
+                    <div key={link.id} className={styles.LinkBlock}>
+                        <p>{link.category}</p>
+                        <a href={link.url} target="_blank" className={styles.Link}>
+                            {link.title}
+                        </a>
+                    </div>
+                ))} */}
+            </div>
         </main>
     );
 };
@@ -45,3 +54,17 @@ export const query = graphql`
         }
     }
 `;
+
+const groupByCategory = (linkArray) => {
+    let categorizedObject = {};
+    linkArray.forEach((linkElement) => {
+        const { category, ...displayedInfo } = linkElement;
+
+        if (category in categorizedObject) {
+            categorizedObject[category].push(displayedInfo);
+        } else {
+            categorizedObject[category] = [displayedInfo];
+        }
+    });
+    return categorizedObject;
+};
